f96e63e67f4a0e88a767342ad6a1e0b11ecd771e
eileenthedev <37980018+eileenthedev@users.noreply.github.com>
2018-04-10 23:24:50 +0800

    Add Progress Text to Seekbar
    Add Progress Text to Seekbar

--- app/src/main/java/com/example/eileen/badmintonmanager/GetPlayersActivity.java
+++ app/src/main/java/com/example/eileen/badmintonmanager/GetPlayersActivity.java
@@ -11,10 +11,33 @@ public class GetPlayersActivity extends AppCompatActivity {
     TextView txtName;
     SeekBar sbLevel;
     Switch swGender;
+    TextView txtSbProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_players);
+
+        sbLevel = (SeekBar) findViewById(R.id.sbLevel);
+        txtSbProgress = (TextView) findViewById(R.id.txtSbProgress);
+        txtName = (TextView) findViewById(R.id.txtName);
+        swGender = (Switch) findViewById(R.id.swGender);
+
+        sbLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txtSbProgress.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
