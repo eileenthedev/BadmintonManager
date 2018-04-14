3422cbbe41b458ceeba5a1d92f847e8ecc878347
eileenthedev <37980018+eileenthedev@users.noreply.github.com>
2018-04-14 22:39:29 +0800

    Fix Navigation View -Click issue
    Fix Navigation View -Click issue

--- app/src/main/java/com/example/eileen/badmintonmanager/GetPlayersActivity.java
+++ app/src/main/java/com/example/eileen/badmintonmanager/GetPlayersActivity.java
@@ -1,11 +1,15 @@
 package com.example.eileen.badmintonmanager;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
@@ -22,6 +26,7 @@ public class GetPlayersActivity extends AppCompatActivity {
     Switch swGender;
     TextView txtSbProgress;
     Button btnAddPlayer;
+    NavigationView navigationView;
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -42,15 +47,40 @@ public class GetPlayersActivity extends AppCompatActivity {
         txtName = (TextView) findViewById(R.id.txtName);
         swGender = (Switch) findViewById(R.id.swGender);
         btnAddPlayer = (Button) findViewById(R.id.btnAddPlayer);
+        navigationView = findViewById(R.id.nav_view);
         setOnLoadListeners();
-
 
     }
 
-    private void setOnLoadListeners(){
+    private void setOnLoadListeners() {
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        switch (menuItem.getTitle().toString()){
+                            case "Get Player List":
+                                Intent myIntent = new Intent(GetPlayersActivity.this, ListPlayerActivity.class);
+                                GetPlayersActivity.this.startActivity(myIntent);
+                                break;
+                                default: break;
+                        }
+
+
+                        return true;
+                    }
+                });
+
 
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -76,10 +106,9 @@ public class GetPlayersActivity extends AppCompatActivity {
         swGender.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     swGender.setText(R.string.lbGenderMale);
-                }
-                else{
+                } else {
                     swGender.setText(R.string.lbGenderFemale);
                 }
             }
@@ -91,17 +120,31 @@ public class GetPlayersActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //saveNewPlayer();
                 BadmintonManagerDbHelper mDbHelper = new BadmintonManagerDbHelper(v.getContext());
-                mDbHelper.saveNewProfile(v.getContext(), txtName.getText().toString(), sbLevel.getProgress(),  swGender.getText().toString());
+                mDbHelper.saveNewProfile(v.getContext(), txtName.getText().toString(), sbLevel.getProgress(), swGender.getText().toString());
             }
-        } );
-
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_get_player_list) {
+            Intent activity_about = new Intent(this, ListPlayerActivity.class);
+            startActivity(activity_about);
+        }
+
+
+        //mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
